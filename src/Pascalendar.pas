a0dde891b9a88fe31cb32d0a647c8eda0f40d9c7
Emilio Devesa <emilio@MacBook-Pro.local>
2023-08-08 18:15:04 +0200

    Ask user for year to print

--- src/Pascalendar.pas
+++ src/Pascalendar.pas
@@ -1,5 +1,22 @@
-program Pascalendar(input, output);
+program Pascalendar(input, output);{
+	Pascalendar
+	Command Line program written in Pascal ISO 10206 (Extended Pascal) that prints any year's calendar
+
+	Emilio Devesa
+}
+
+function getYear: integer;
+var year: integer;
+begin
+    repeat
+        write('Type Year (format: yyyy): ');
+        readln(year);
+        if (year<1)
+        then write('Invalid year. ');
+    until (1<=year) and (year<=MAXINT);
+    getYear:=year;
+end;
 
 begin
-	writeln("Hello World!");
+	writeln(getYear);
 end.
