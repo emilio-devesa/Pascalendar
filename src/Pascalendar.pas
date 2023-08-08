ca2b13e0ed87c4af656721b92ff1f07fa38d1bdb
Emilio Devesa <emilio@MacBook-Pro.local>
2023-08-08 18:19:06 +0200

    getDayOfTheWeek function implemented using Gauss' formula

--- src/Pascalendar.pas
+++ src/Pascalendar.pas
@@ -10,6 +10,18 @@ begin
 	isLeapYear:=((year mod 4 = 0) and not (year mod 100 = 0)) or (year mod 400 = 0);
 end;
 
+function getDayOfTheWeek(year:integer; month:integer; day: integer): integer;
+{ https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week#Gauss's_algorithm }
+var offsetOfCommonYear: array [1..12] of integer value [1:0; 2:3; 3:3; 4:6; 5:1; 6:4; 7:6; 8:2; 9:5; 10:0; 11:3; 12:5];
+	offsetOfLeapYear: array [1..12] of integer value [1:0; 2:3; 3:4; 4:0; 5:2; 6:5; 7:0; 8:3; 9:6; 10:1; 11:4; 12:6];
+	offset: integer;
+begin
+    if isLeapYear(year)
+    then offset:=offsetOfLeapYear[month]
+    else offset:=offsetOfCommonYear[month];
+    getDayOfTheWeek:=(day + offset + 5*((year-1) mod 4) + 4*((year-1) mod 100) + 6*((year-1) mod 400)) mod 7;
+end;
+
 function getYear: integer;
 var year: integer;
 begin
